75a521b6aa55cbca4159548bfe5dc6678e0cc123
TT <TT@126.com>
2015-12-29 15:36:00 +0800

    修炼开始

---
+++ kaka/kaka/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kaka
+//
+//  Created by apple on 15/12/29.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kaka/kaka/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kaka
+//
+//  Created by apple on 15/12/29.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
